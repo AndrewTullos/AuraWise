c9dc2695a7c1cda8b85ec2bf3a5d282298f1239d
Drew Tullos <tullos.andrew@gmail.com>
2024-06-17 17:13:31 -0500

    quick fix a tag route

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -46,7 +46,7 @@ export default function Example() {
 						<div className="relative flex h-16 justify-between">
 							<div className="relative z-10 flex px-2 lg:px-0">
 								<div className="flex flex-shrink-0 items-center">
-									<a href="./">
+									<a href="https://andrewtullos.github.io/AuraWise/">
 										<Icon />
 									</a>
 								</div>
